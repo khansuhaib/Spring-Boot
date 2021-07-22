b12f8932142d7e0acb48b976ce46b1c4df99450a
khansuhaib <khansuhaib@gmail.com>
2021-07-21 22:21:37 -0400

    master

---
+++ src/main/java/com/ibm/example/testspring/TestspringApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.example.testspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestspringApplication.class, args);
+	}
+}
